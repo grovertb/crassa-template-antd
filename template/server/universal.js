ba6725a7f7b2cd444532e290680106f970561484
GroverTB <grover.jtb@gmail.com>
2019-06-18 09:17:44 -0500

    [ UPDATE ] universal configure

--- template/server/universal.js
+++ template/server/universal.js
@@ -1,14 +1,14 @@
 import { renderToString } from 'react-dom/server'
 import { ServerStyleSheet } from 'styled-components'
 
-export const setRenderUniversal = (locals, app, extractor) => {
+export const setRenderUniversal = (locals, app) => {
   const sheet = new ServerStyleSheet()
   try {
     const { htmlData } = locals
     const renderString = renderToString(sheet.collectStyles(app))
 
     return {
-      prevHtml: htmlData.replace('<head>', `<head><style>html { margin:0px; padding:0px }</style>${extractor.getStyleTags()}${sheet.getStyleTags()}`),
+      prevHtml: htmlData.replace('<head>', `<head><style>html { margin:0px; padding:0px }</style>${sheet.getStyleTags()}`),
       renderString
     }
   } catch (error) {
